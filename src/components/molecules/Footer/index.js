ed66dcadc71bd0ae9601dbc58c97d17ca5eb3eb2
jodangadeharshal <81670012+jodangadeharshal@users.noreply.github.com>
2022-07-16 18:51:32 +0530

    Add files via upload

---
+++ src/components/molecules/Footer/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import './style.css';
+
+const Footer = (props) => {
+  return (
+    <footer className="Footer">
+      <section>
+        {' '}
+        <span className="Footer__Heart"></span> {' '}
+        <a
+          href="hhhhhhaaaarrrrrssshhhaaalll"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+        
+        </a>
+      </section>
+
+      <section className="Footer__Items">
+        <a
+          href="hhhhhhaaaarrrssshhhaaallll"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Demo
+        </a>
+        <a
+          href="hhhhhaaaarrrssshhhalll"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="Footer__Link"
+        >
+          Code
+        </a>
+      </section>
+    </footer>
+  );
+};
+
+export default Footer;
